9b38425e154536ff59dd524595e2d3489d03b0d9
buhairong <22858111@qq.com>
2020-07-21 09:33:43 +0800

    1

--- part1_vue/8.ts_vue/vue-study-web20-ts/src/App.vue
+++ part1_vue/8.ts_vue/vue-study-web20-ts/src/App.vue
@@ -3,41 +3,39 @@
   <div id="app">
     <input type="text" v-model="username" placeholder="请输入用户名">
     <span>{{username}}</span>
-    <span>{{msg}}</span>
     <button @click="submit">提交用户名</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
-function validate(target: any, name: string, descriptor: PropertyDescriptor) {
-      const fn = descriptor.value
-      descriptor.value = function(e: KeyboardEvent) {
-        console.log(this)
-        if(this.username && this.username.trim()) {
-          let reg = /^[a-zA-Z0-9_]{6,10}$/
-          let value = this.username.trim()
-          if(value && !reg.test(value)) {
-             fn.call(this, '用户名请输入6至10位的字母数字以及下划线')
-          }  
-        }             
-      }    
-}
+// function validate(target: any, name: string, descriptor: PropertyDescriptor) {
+//       const fn = descriptor.value
+//       descriptor.value = function(e: KeyboardEvent) {
+//         console.log(this)
+//         if(this.username && this.username.trim()) {
+//           let reg = /^[a-zA-Z0-9_]{6,10}$/
+//           let value = this.username.trim()
+//           if(value && !reg.test(value)) {
+//              fn.call(this, '用户名请输入6至10位的字母数字以及下划线')
+//           }  
+//         }             
+//       }    
+// }
 
+@Component
 export default class App extends Vue {
-  username:string =  ''  
-  msg:string =  '' 
+  // 类的属性将直接转换为data
+  username!: string
 
   constructor() {
     super()
     this.submit = this.submit.bind(this)     
   }
 
-  @validate
   submit(msg: string) {    
     console.log(this)
-    this.msg = 'abcdef'
   }  
 }
 </script>
