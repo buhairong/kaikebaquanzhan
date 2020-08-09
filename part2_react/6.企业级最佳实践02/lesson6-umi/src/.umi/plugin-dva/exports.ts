2d70873f531d0c1f8a253afa9b2b0b270d292a88
buhairong <22858111@qq.com>
2020-08-09 14:13:16 +0800

    1

--- part2_react/6.企业级最佳实践02/lesson6-umi/src/.umi/plugin-dva/exports.ts
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-
-export { connect, useDispatch, useStore, useSelector } from '/Users/gaoshaoyun/workspace/kkb-react/lesson6-umi/node_modules/dva';
-export { getApp as getDvaApp } from './dva';
--- part2_react/6.企业级最佳实践02/lesson6-umi/src/.umi/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from '/Users/gaoshaoyun/workspace/kkb-react/lesson6-umi/node_modules/react-helmet';
