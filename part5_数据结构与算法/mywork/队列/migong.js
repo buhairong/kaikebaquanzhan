1e0741030c3d80eb52e701a0b536f840cb85196a
buhairong <22858111@qq.com>
2020-09-16 08:47:11 +0800

    1

--- part5_数据结构与算法/mywork/队列/migong.js
+++ part5_数据结构与算法/mywork/队列/migong.js
@@ -19,8 +19,51 @@ let Position = function(x, y) {
     this.y = y
 }
 
-function findPath(maze_array, startPos, endPos) {
+class findPath {
+    constructor(maze_array, startPos, endPos) {
+        this.maze_array = maze_array
+        this.startPos = startPos
+        this.endPos = endPos
+        this.rowLen = this.maze_array.length
+        this.colLen = this.maze_array[0].length
+        this.step = 0
+        this.queue[this.step] = [this.startPos]
 
+        this.createQueue()
+        
+    }
+
+    createQueue() {
+        this.findNextNode()
+    }
+
+    // 根据每个节点上下左右寻找可以通行的下一个节点
+    findNextNode() {
+        let step = this.step++        
+
+        for (let i = 0; i < this.queue[this.step].length; i++) {
+            let node = this.queue[this.step][i];
+
+            // 上边的节点
+            if(node.y > 0 && this.maze_array[node.y - 1][node.x] === 0) {
+                let nextNode = new Position(node.x, node.y-1)
+                this.queue[step].push({nextNode})
+            }
+
+            // 下边的节点
+            if(node.y < this.rowLen-1 && this.maze_array[node.y + 1][node.x] === 0) {
+                let nextNode = new Position(node.x, node.y+1)
+                this.queue[step].push({nextNode})
+            }
+
+            // 左边的节点
+            if(node.y > 0 && this.maze_array[node.y - 1][node.x] === 0) {
+                let nextNode = new Position(node.x, node.y+1)
+                this.queue[step].push({nextNode})
+            }
+            
+        }              
+    }    
 }
 
 
