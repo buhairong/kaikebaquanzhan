4168984965b9a636e895484d4a7facde19bcc49a
buhairong <22858111@qq.com>
2020-11-06 17:06:35 +0800

    1

---
+++ 实战/断点续传/project-full/front/pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-form class="login-form" :rules="rules">
+      <el-form-item props="email">
+        <span>
+          <i class="el-icon-mobile" />
+        </span>
+        <el-input placeholder="邮箱"></el-input>
+      </el-form-item>
+      <el-form-item props="passwd">
+        <span>
+          <i class="el-icon-lock" />
+        </span>
+        <el-input placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item props="captcha">
+        <el-input placeholder="验证码"></el-input>
+        <img :src="captchaUrl" @click="updateCaptcha" >
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'login',
+  data () {
+    return {
+      rules: {},
+      captchaUrl: "/api/captcha?_t=" + new Date().getTime()
+    }
+  },
+  methods:{      
+    updateCaptcha () {
+      this.captchaUrl = "/api/captcha?_t=" + new Date().getTime()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.login-form {
+    width: 800px;
+    margin: 50px auto;
+}
+</style>
---
+++ 实战/断点续传/项目前期知识点准备/egg/egg/app/service/user.js
@@ -0,0 +1,15 @@
+const {Service} = require('egg')
+
+class UserService extends Service {
+    async getAll() {
+        // return [
+        //     {
+        //         name: 'UserService'
+        //     }
+        // ]
+
+        return await this.ctx.model.User.findAll()
+    }
+}
+
+module.exports = UserService
